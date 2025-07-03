52340ccf4fba8bea78c446e46ffa90d479c9d1e8
Milan Dhondt <milan.dhondt@student.hogent.be>
2025-07-03 15:57:37 +0200

    Made the alignemnt of the footer look better

--- Portfolio/src/components/General Components/Footer/BigFooter.jsx
+++ Portfolio/src/components/General Components/Footer/BigFooter.jsx
@@ -2,13 +2,15 @@ import LinkSection from "./LinkSection";
 
 const BigFooter = () => {
   return (
-    <div className="flex flex-col md:flex-row items-center justify-between py-8 max-w-7xl">
+    <div className="flex flex-col md:flex-row items-center justify-between py-8 max-w-7xl mx-auto px-4">
       <img
         src="/logoRondZB.png"
         alt="Logo"
         className="w-32 h-32 object-contain border border-[#5F0077] rounded-full"
       />
-      <LinkSection />
+      <div className="md:ml-auto md:mr-0 mt-6 md:mt-0">
+        <LinkSection />
+      </div>
     </div>
   );
 };
