1c70b1a6ebb74d458f06f078d7a25f91de35f18d
Milan Dhondt <milan.dhondt@student.hogent.be>
2025-07-04 16:19:27 +0200

    Finished contact section

--- Portfolio/src/components/About me/ContactSection.jsx
+++ Portfolio/src/components/About me/ContactSection.jsx
@@ -1,6 +1,18 @@
+import Spacer from '../Spacer';
+import CustomButton from '../General Components/CustomButton';
+
 const ContactSection = () => {
   return (
-    <div>ContactSection</div>
+    <div>
+      <div className="bg-[#F2F2F2] border rounded-2xl p-10 m-10 mb-0 text-center">
+        <h1 className='text-5xl mb-4'>Extra info nodig?</h1>
+        <p className='text-xl'>
+          Ik ben altijd geïnteresseerd in nieuwe kansen of projecten. Heb je een vraag, wil je iets weten, of wil je samenwerken? Stuur mij gerust een berichtje!
+        </p>
+        <CustomButton text={"Contact"} to={"/contact"} />
+      </div>
+      <Spacer black={true} />
+    </div>
   )
 }
 
