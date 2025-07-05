4b2a4cb15d1b19f27b3f8b14aa3245d6ef14ecaf
Milan Dhondt <milan.dhondt@student.hogent.be>
2025-07-05 14:47:45 +0200

    Changed icon colours to purple

--- Portfolio/src/components/General Components/Footer/LinkSection.jsx
+++ Portfolio/src/components/General Components/Footer/LinkSection.jsx
@@ -4,24 +4,24 @@ const LinkSection = () => {
   return (
     <div className="space-y-4">
       <div className="flex items-center gap-2">
-        <FaPhone />
+        <FaPhone className="text-[#5F0077]" />
         <a href="tel:+32478410722" className="hover:underline">+32 478 41 07 22</a>
       </div>
 
       <div className="flex items-center gap-2">
-        <FaEnvelope />
+        <FaEnvelope className="text-[#5F0077]" />
         <a href="mailto:dhondt.milan@protonmail.com" className="hover:underline">dhondt.milan@protonmail.com</a>
       </div>
 
       <div className="flex items-center gap-2">
-        <FaLinkedin />
+        <FaLinkedin className="text-[#5F0077]" />
         <a href="https://www.linkedin.com/in/milan-dhondt/" target="_blank" rel="noopener noreferrer" className="hover:underline">
           @milan-dhondt
         </a>
       </div>
 
       <div className="flex items-center gap-2">
-        <FaGithub />
+        <FaGithub className="text-[#5F0077]" />
         <a href="https://github.com/milandhondt" target="_blank" rel="noopener noreferrer" className="hover:underline">
           @milandhondt
         </a>
