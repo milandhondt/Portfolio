3fc9cf34723b39e7b7e0fc8741c6aa1b2dc941b6
Milan Dhondt <milan.dhondt@student.hogent.be>
2025-07-05 10:21:59 +0200

    Merged the text section with the other section of text and finished it

---
+++ Portfolio/src/components/About me/AboutDebutSection.jsx
@@ -0,0 +1,23 @@
+import Spacer from "../Spacer";
+import Title from "../General Components/Title";
+
+const AboutDebutSection = () => {
+  return (
+    <>
+      <section className="bg-[#333333] flex flex-col items-center justify-center text-center px-6">
+        <img
+          src="/fotoTitanic.jpg"
+          className="rounded-full h-64 w-64 sm:h-72 sm:w-72 md:h-80 md:w-80 object-cover shadow-lg mb-6"
+          alt="Portretfoto van Milan Dhondt"
+        />
+        <Title text={"Hallo, ik ben Milan Dhondt"} pos={"C"} white={true} />
+        <p className="text-xl text-white mt-3 max-w-xl">
+          Ik ben een 21-jarige student full-stack development aan HOGENT. Tijdens mijn opleiding heb ik een sterke interesse ontwikkeld in zowel frontend als backend development. Ik werk graag in teamverband, omdat ik de verschillende input van mensen enorm leuk en interessant vind.
+        </p>
+      </section>
+      <Spacer big={false} black={true} />
+    </>
+  );
+};
+
+export default AboutDebutSection;
--- Portfolio/src/pages/AboutPage.jsx
+++ Portfolio/src/pages/AboutPage.jsx
@@ -1,14 +1,14 @@
-import TextSection from '../components/About me/TextSection'
 import SkillsSection from '../components/About me/Skills/SkillsSection'
 import ExperienceSection from '../components/About me/Experience/ExperienceSection'
 import EducationSection from '../components/About me/Education/EducationSection'
 import ContactSection from '../components/About me/ContactSection'
+import AboutDebutSection from '../components/About me/AboutDebutSection'
+
 
 const AboutPage = () => {
   return (
     <div className='bg-[#333333]'>
-      <p className="bg-[#F2F2F2]">Hier komt een leuke foto met klein beetje info</p>
-      <TextSection />
+      <AboutDebutSection />
       <SkillsSection />
       <ExperienceSection />
       <EducationSection />
