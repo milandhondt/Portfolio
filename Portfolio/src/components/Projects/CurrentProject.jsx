22f88e2a762686527637bae8328155079f53ba4e
Milan Dhondt <milan.dhondt@student.hogent.be>
2025-09-01 18:24:32 +0200

    Styling fix voor current project sectie

--- Portfolio/src/components/Projects/CurrentProject.jsx
+++ Portfolio/src/components/Projects/CurrentProject.jsx
@@ -1,8 +1,8 @@
 const CurrentProject = ({ title, text }) => {
   return (
-    <div className="p-15 py-5 m-15 text-center">
-      <h1 className="text-5xl font-bold mb-8">Huidig project</h1>
-      <p className="text-2xl">
+    <div className="px-4 sm:px-8 lg:px-16 py-5 text-center">
+      <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold mb-6 sm:mb-8">Huidig project</h1>
+      <p className="text-lg sm:text-xl lg:text-2xl">
         Momenteel werk ik aan <b>{title}</b>. {text} Zodra de applicatie klaar is, zal de repository beschikbaar zijn op&nbsp;
         <a href="https://github.com/milandhondt" className="underline">mijn GitHub</a>.
       </p>
